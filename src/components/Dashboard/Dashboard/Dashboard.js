07f795556a2acc56f93a9e60e43c75ec2d3fbd9c
farukwebcode21 <farukahmadmk@yahoo.com>
2022-02-11 22:42:51 +0600

    running

--- src/components/Dashboard/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard/Dashboard.js
@@ -5,22 +5,8 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const Dashboard = () => {
   return (
-    <div className='topbar'>
-      <div className='topbarWrapper'>
-        <div className='topLeft'>
-          <div  className='iconDashboard'>
-             <span> <DashboardIcon/></span>
-              <span className='logo'>Dashboard</span>
-          </div>
-          
-        </div>
-        <div className='topRight'>
-          <div className='topbarIconContainer'>
-          <span className='username'>Mr. Robin<br/>Admin</span>
-          <span className='userimage'> <AccountCircleIcon/></span>
-          </div>
-        </div>
-      </div>
+    <div className='container'>
+      
     </div>
   )
 }
